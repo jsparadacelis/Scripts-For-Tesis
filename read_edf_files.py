05aa20e9a73487308ecb8fd7eacf3fbd50f505ac
fredyvargas <fredyvargas@bitbucket.org>
2019-01-10 09:19:03 -0500

    Commit for read an calculate number of channels

---
+++ read_edf_files.py
@@ -0,0 +1,28 @@
+import os
+path = '/home/ticsocial/Descargas/'
+
+# valida la extensión del fichero
+def is_edf_file(file_name):
+    return file_name[len(file_name)-3:len(file_name)] == "edf"
+
+def calc_auto(path):
+    # Encontrar los archivos edf dentro del directorio
+    files = os.listdir(path)
+    edf_files = [name for name in files if is_edf_file(name)]
+    
+    #iterando sobre los ficheros D:
+    for file_name in edf_files:
+        
+        edf_file = pyedflib.EdfReader(path+file_name)
+        
+        num_signals = edf_file.signals_in_file
+        signal_labels = edf_file.getSignalLabels()
+        
+        channels = signal_labels[0:21] + signal_labels[24:26]
+        print(len(channels))
+
+        
+        
+        
+
+calc_auto(path)
